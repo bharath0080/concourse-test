1db1a16257e56422c38ace64b5438e671590293e
bharath0080 <bharathforany@gmail.com>
2019-04-03 15:10:48 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,4 +1,11 @@
-println "Insite the container"
-def cmd = "groovy --version"
-def output = cmd.execute().text
-println output
+class Main {
+    static void main(String... args) {
+        args.each {
+            println it
+        }
+        println "Insite the container"
+        def cmd = "/app/home/jenkins/groovy-2.4.7/bin/groovy --version"
+        def output = cmd.execute().text
+        println output
+    }
+}
