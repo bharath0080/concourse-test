f12372371cf28631f9f33caf83809a573c2344a1
bharath0080 <bharathforany@gmail.com>
2019-04-04 10:36:46 +0530

    Update executegitcommands.sh

--- executegitcommands.sh
+++ executegitcommands.sh
@@ -2,7 +2,7 @@ yum install git -y && git clone concourse-test concourse-test-new && \
 cd ./concourse-test-new && \
 git config --global user.email "bharath@gmail.com" && \
 git config --global user.name "bharath" && \
-echo "test" > test.txt && \
+echo "test" >> test.txt && \
 git status && \
 git add --all && \
 git commit -m "tes t"
