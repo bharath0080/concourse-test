17c5c7df0ea399588202710a1ea4627f7b1cae4f
bharath0080 <bharathforany@gmail.com>
2019-04-04 12:42:11 +0530

    Update executegitcommands.sh

--- executegitcommands.sh
+++ executegitcommands.sh
@@ -1,6 +1,6 @@
 yum install git -y && git clone concourse-test concourse-test-new && \
 cd ./concourse-test-new && \
-sed '$e echo COPY * /var/tmp' Dockerfile > Dockerfile_1 && \
+sed '$e echo COPY ./* /var/tmp' Dockerfile > Dockerfile_1 && \
 mv Dockerfile_1  Dockerfile && \
 sed '$e echo RUN yum install -y' Dockerfile > Dockerfile_1 && \
 mv Dockerfile_1  Dockerfile && \
