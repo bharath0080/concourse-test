2694b5895cf1718a39d7fe14ca4958c7d259d240
bharath0080 <bharathforany@gmail.com>
2019-04-04 12:19:34 +0530

    Update executegitcommands.sh

--- executegitcommands.sh
+++ executegitcommands.sh
@@ -1,8 +1,13 @@
 yum install git -y && git clone concourse-test concourse-test-new && \
 cd ./concourse-test-new && \
+sed '$e echo COPY ./concourse-test /var/tmp' Dockerfile > Dockerfile_1 && \
+mv Dockerfile_1  Dockerfile && \
+sed '$e echo RUN yum install -y' Dockerfile > Dockerfile_1 && \
+mv Dockerfile_1  Dockerfile && \
 git config --global user.email "bharath@gmail.com" && \
 git config --global user.name "bharath" && \
 echo "test" >> test.txt && \
 git status && \
 git add --all && \
 git commit -m "tes t"
+cat Dockerfile
