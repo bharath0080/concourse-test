b0449e540d18e2367d3b3a8a430a536a14aefede
bharath0080 <bharathforany@gmail.com>
2019-06-11 14:45:05 +0530

    Update executegitcommands.sh

--- executegitcommands.sh
+++ executegitcommands.sh
@@ -1,13 +1,13 @@
 yum install git -y && git clone concourse-test concourse-test-new && \
 cd ./concourse-test-new && \
-rm -rf testing && \
-mkdir testing && \
-touch testing/test{1..4}.txt && \
-sed '$e echo COPY ./testing/* /var/tmp/' Dockerfile > Dockerfile_1 && \
-mv Dockerfile_1  Dockerfile && \
+#rm -rf testing && \
+#mkdir testing && \
+#touch testing/test{1..4}.txt && \
+#sed '$e echo COPY ./testing/* /var/tmp/' Dockerfile > Dockerfile_1 && \
+#mv Dockerfile_1  Dockerfile && \
+echo "test" >> version && \
 git config --global user.email "bharath@gmail.com" && \
 git config --global user.name "bharath" && \
-echo "test" >> test.txt && \
 git status && \
 git add --all && \
 git commit -m "tes t"
